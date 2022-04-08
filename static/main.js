e7eb72f38651bd0c9dc382f854bf90e6fe3d9701
Dima Tyshchenko <dima.tyshchenko@ekreative.com>
2022-04-08 18:40:17 +0300

    Added static files for frontend client

---
+++ static/main.js
@@ -0,0 +1,38 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    title: 'Nestjs Websockets Chat',
+    name: '',
+    text: '',
+    messages: [],
+    socket: null,
+  },
+  methods: {
+    sendMessage() {
+      if(this.validateInput()) {
+        const message = {
+          name: this.name,
+          text: this.text
+        };
+      this.socket.emit('createMessage', message);
+      this.text = '';
+      }
+    },
+    receivedMessage(message) {
+      this.messages.push(message);
+    },
+    validateInput() {
+      return this.name.length > 0 && this.text.length > 0;
+    }
+  },
+  created() {
+    this.socket = io('http://localhost:8080', {
+      query: {
+        conversationID: '6246cd493a7e872c53582fcc',
+      }
+    });
+    this.socket.on('newMessage', (message) => {
+      this.receivedMessage(message);
+    });
+  },
+});
