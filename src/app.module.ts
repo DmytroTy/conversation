dcf3f95370891735a0178d026f617e7793662ed1
Dima Tyshchenko <dima.tyshchenko@ekreative.com>
2022-04-13 16:33:30 +0300

    Added logger-winston module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { ConversationsModule } from './conversations/conversations.module';
+import { LoggerModule } from './logger/logger.module';
 import { MessagesModule } from './messages/messages.module';
 import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -27,6 +28,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     AuthModule,
     ConversationsModule,
+    LoggerModule,
     MessagesModule,
     UsersModule,
   ],
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import { LoggerWinston } from './logger/logger-winston.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { bufferLogs: true });
+
+  app.useLogger(app.get(LoggerWinston));
 
   app.useStaticAssets(join(__dirname, '..', 'static'));
 
